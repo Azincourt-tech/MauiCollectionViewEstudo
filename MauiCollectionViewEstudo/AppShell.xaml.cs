6994571064c53201c69ea48ef7c4a7eb975bac9c
Willian <williandasilvaalmeida3@gmail.com>
2026-02-02 20:58:39 -0300

    Adicionar arquivos de projeto.

---
+++ MauiCollectionViewEstudo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiCollectionViewEstudo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
